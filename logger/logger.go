4e0bcb4cbb37ae97d79058c019882706d4e7681c
Anacardo <pedro.fortunato.89@gmail.com>
2024-07-13 19:24:39 +0100

    Sends mails but subject goes into body

--- logger/logger.go
+++ logger/logger.go
@@ -12,15 +12,15 @@ var (
 )
 
 func CreateLogger() error {
-	infoFile, err := os.OpenFile("/logs/info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	infoFile, err := os.OpenFile("logs/info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal("Cannot access INFO log file:", err)
 	}
-	warnFile, err := os.OpenFile("/logs/warn.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	warnFile, err := os.OpenFile("logs/warn.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal("Cannot access WARN log file:", err)
 	}
-	errorFile, err := os.OpenFile("/warns/error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	errorFile, err := os.OpenFile("logs/error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatal("Cannot access ERROR log file:", err)
 	}
